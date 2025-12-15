0959887516d599a351bcfd03ad2c02f48d2a9a30
Andrei Suvorau <suvri4@gmail.com>
2025-12-15 22:45:38 +0100

    Allow zero-length comments

--- api/models/comment_model.py
+++ api/models/comment_model.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 class CommentBase(BaseModel):
     ChangeNumber: int = Field(..., ge=0)
     UserName: str = Field(..., min_length=1, max_length=128)
-    Text: str = Field(..., min_length=1, max_length=1024)
+    Text: str = Field(..., max_length=1024)
     Project: str = Field(..., min_length=1, max_length=128)
 
 class CommentRequest(CommentBase):
